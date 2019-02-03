4470874082b57aa04c356278a22b99e1778a45b0
Reddy <lewismd3283@gmail.com>
2019-02-02 22:25:25 -0600

    Finally fucking finished web caesar!

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="POST">
             <label for="rot">Rotate by:</label>
             <input type="text" name="rot" value="0">
-            <textarea type="text" name="text"></textarea>
+            <textarea type="text" name="text">{0}</textarea>
             <button type="submit">Submit Query</button>
         </form>
     </body>
@@ -38,13 +38,13 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = str(request.form['text'])
     encrypted = rotate_string(text, rot)
-    return "<h1>" + encrypted + "</h1>"
+    return form.format(encrypted)
 
 app.run()
